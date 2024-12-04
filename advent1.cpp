03112c917092661fc92051e38e09e95618c1c0a8
Fanny BOUSSARD <boussard.fanny@gmail.com>
2024-12-04 11:51:26 +0100

    first commit

---
+++ advent1.cpp
@@ -0,0 +1,17 @@
+// sort both lists
+// pair up the smallest numbers in the left list with the smallest in the right list
+// = pair up the numbers
+// calculate b - a
+// sum the difference
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    fopen("input.txt", "r");
+    if (!is_open("input.txt"))
+    std::getline ()
+
+}
