7a59f29d4ffe4607c06fc895607b1a5607d5deb7
Fanny BOUSSARD <boussard.fanny@gmail.com>
2024-12-07 18:25:10 +0100

    init advent 3

---
+++ day3/advent3.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <numeric>
+#include <iterator>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "error: usage " << argv[0] << "input file" << std::endl;
+        return 1;
+    }
+
+    
+}
